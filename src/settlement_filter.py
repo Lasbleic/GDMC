8333ae11ec03b790c833497a81afad8020bdc8b6
lasbleic <corentin.lasbleis@grenoble-inp.fr>
2020-05-04 11:37:30 +0200

    Set settlement_filter.py basic interface to be considered as a filter by MCEdit

---
+++ src/settlement_filter.py
@@ -0,0 +1,12 @@
+# Name to display in MCEdit filter menu
+displayName = "Create a settlement"
+
+# Dictionnary representing different options
+inputs = ()
+
+
+# Necessary function to be considered as a filter by MCEdit. This is the function that will be executed when the filter
+# will be applied
+
+def perform(level, box, options):
+    print("Hello World!")
