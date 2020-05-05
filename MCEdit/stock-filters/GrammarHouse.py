9db987fcd18ed93b40b4eb1615395188491b35b9
CharlieMDRz <CharlieMDRz@github.com>
2020-05-05 21:20:43 +0200

    grammatical house: added stair roof

--- MCEdit/stock-filters/GrammarHouse.py
+++ MCEdit/stock-filters/GrammarHouse.py
@@ -17,7 +17,7 @@ inputs = (
 
 def perform(level, box, options):
     print('building house in box', box)
-    palette = {'base': alphaMaterials.Cobblestone, 'walls': alphaMaterials.WoodPlanks, 'roof': alphaMaterials.Brick}
+    palette = {'base': alphaMaterials.Cobblestone, 'wall': alphaMaterials.WoodPlanks, 'roof': alphaMaterials.BrickStairs}
     house = HouseSymbol()
     house.generate(level, box, palette)
 
@@ -50,7 +50,8 @@ class BaseSymbol(Symbol):
 
     def generate(self, level, box, palette):
         h = compute_height_map(level, box)
-        box = BoundingBox((box.minx, np.min(h), box.minz), (box.width, np.mean(h) + 1 - np.min(h), box.length))
+        print(h, np.mean(h) + 1)
+        box = BoundingBox((box.minx, np.min(h), box.minz), (box.width, np.mean(h) + 2 - np.min(h), box.length))
         print('filling base', box)
         fillBlocks(level, box, palette['base'])
         return Symbol.generate(self, level, box, palette)
@@ -61,17 +62,47 @@ class StoreySymbol(Symbol):
     def generate(self, level, box, palette):
         box2 = BoundingBox((box.minx, box.maxy, box.minz), (box.width, 4, box.length))
         print('filling walls', box2)
-        fillBlocks(level, box2, palette['walls'])
+        fillBlocks(level, box2, palette['wall'])
         fillBlocks(level, box2.expand(-1, 0, -1), alphaMaterials.Air)
         return Symbol.generate(self, level, box2, palette)
 
 
 class RoofSymbol(Symbol):
     def generate(self, level, box, palette):
-        box2 = BoundingBox((box.minx, box.maxy, box.minz), (box.width, 1, box.length))
-        print('filling roof', box2)
-        fillBlocks(level, box2, palette['roof'])
-        return Symbol.generate(self, level, box2, palette)
+        box = BoundingBox((box.minx, box.maxy, box.minz), (box.width, 1, box.length))
+        print('filling roof', box)
+        r = random()
+        t = float(box.length**2) / (box.width**2 + box.length**2)
+        print(r, t)
+        roof_in_width = r > t  # higher probability to have a lower & longer triangular roof
+
+        if roof_in_width:
+            for dz in xrange(box.length//2):
+                # north line
+                roof_box = BoundingBox((box.minx, box.miny+dz, box.minz+dz), (box.width, 1, 1))
+                fillBlocks(level, roof_box, alphaMaterials[palette['roof'].ID, 2])  # this way to access oriented blocks is ugly
+
+                # wood fill
+                for x in [box.minx, box.maxx-1]:
+                    roof_box = BoundingBox((x, box.miny+dz, box.minz+dz+1), (1, 1, box.length - 2*(dz+1)))
+                    fillBlocks(level, roof_box, palette['wall'])
+
+                # south line
+                roof_box = BoundingBox((box.minx, box.miny+dz, box.maxz-dz-1), (box.width, 1, 1))
+                fillBlocks(level, roof_box, alphaMaterials[palette['roof'].ID, 3])
+
+        else:
+            for dx in xrange(box.width//2):
+                roof_box = BoundingBox((box.minx+dx, box.miny+dx, box.minz), (1, 1, box.length))
+                fillBlocks(level, roof_box, alphaMaterials[palette['roof'].ID, 0])
+
+                for z in [box.minz, box.maxz-1]:
+                    roof_box = BoundingBox((box.minx+dx+1, box.miny+dx, z), (box.width - 2*(dx+1), 1, 1))
+                    fillBlocks(level, roof_box, palette['wall'])
+
+                roof_box = BoundingBox((box.maxx-dx-1, box.miny+dx, box.minz), (1, 1, box.length))
+                fillBlocks(level, roof_box, alphaMaterials[palette['roof'].ID, 1])
+        return Symbol.generate(self, level, box, palette)
 
 
 
