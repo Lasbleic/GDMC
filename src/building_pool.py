4c303fdd15d26ad597a62c524d4c47f654c01348
CharlieMDRz <CharlieMDRz@github.com>
2020-05-09 21:19:02 +0200

    added empty classes for first increment project

---
+++ src/building_pool.py
@@ -0,0 +1,7 @@
+class BuildingPool:
+    def __init__(self):
+        self.settlement_size = 0
+
+    def next_building(self):
+        self.settlement_size += 1
+        return None
---
+++ src/flat_settlement.py
@@ -0,0 +1,10 @@
+class FlatSettlement:
+    """
+    Intermediate project: generate a realist village on a flat terrain
+    """
+
+    def __init__(self, box):
+        self.limits = box
+
+    def generate(self, level):
+        pass
