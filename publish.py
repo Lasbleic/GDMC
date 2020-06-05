b9cdf3b17615f1f61fad4a1cbd7b24ee537e8bc8
Charlie Amaudruz <cfamaudruz@gmail.com>
2020-06-06 01:06:53 +0200

    Test filters to replace __main__ functions and hopefully avoid circular imports

--- publish.py
+++ publish.py
@@ -10,5 +10,12 @@ try:
     su.rmtree("./MCEdit/stock-filters/settlement_filter")
 except OSError:
     print("Didn't copy then delete stock-filters/settlement_filter as it does not exist")
-
 su.copytree("./src", "./MCEdit/stock-filters/settlement_filter")
+
+try:
+    su.rmtree("./MCEdit/stock-filters/custom_test_filters")
+except OSError:
+    print("Didn't copy then delete stock-filters/custom_test_filters as it does not exist")
+su.copytree("./test", "./MCEdit/stock-filters/custom_test_filters")
+
+
---
+++ test/test_house_generator.py
@@ -0,0 +1,17 @@
+# Name to display in MCEdit filter menu
+
+from generation import TransformBox, ProcHouseGenerator
+from map.maps import Maps
+from pymclevel import BoundingBox, MCLevel
+
+displayName = "House generator test filter"
+
+inputs = ()
+
+
+def perform(level, box, options):
+    # type: (MCLevel, BoundingBox, dict) -> None
+    box = TransformBox(box)
+    maps = Maps(level, box)
+    gen = ProcHouseGenerator(box)
+    gen.generate(level, height_map=maps.height_map)
